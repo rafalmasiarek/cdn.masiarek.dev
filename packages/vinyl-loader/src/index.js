1654f0b9be94d8ba9b11713e8fafc3fb87001658
Rafał Masiarek <rafal@masiarek.pl>
2026-01-26 00:03:40 +0100

    feat: add twemoji-windows

--- packages/vinyl-loader/src/index.js
+++
@@ -1,7 +0,0 @@
-// Example package entrypoint.
-// Replace with your real script(s).
-(function () {
-  window.VinylLoader = window.VinylLoader || {};
-  window.VinylLoader.version = "dev";
-  console.log("[vinyl-loader] loaded");
-})();
